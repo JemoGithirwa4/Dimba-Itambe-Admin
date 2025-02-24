5a91948ee9771c33f702adb4e186995777e43b28
James Wainaina Githirwa <jagithirwa@gmail.com>
2025-02-24 21:02:42 +0300

    Blog server side complete

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ db.connect();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+//Latest page routes
 let articles = []
 
 app.get("/", async (req, res) => {
@@ -35,8 +36,110 @@ app.get("/", async (req, res) => {
     }
 });
 
+//Create new article post
+app.post("/add-post", async (req, res) => {
+    const { postTitle, postBody, postImageURL, authorName } = req.body;
+
+    try {
+        await db.query("INSERT INTO articles (title, content, imageurl, authorname) VALUES ($1, $2, $3, $4)", [postTitle, postBody, postImageURL, authorName]);
+        res.redirect("/");
+    } catch (err) {
+        console.log(err);
+        res.status(500).send("Server Error");
+    }
+});
+
+app.get("/view/:id", async(req, res) => {
+    try {
+        const articleId = req.params.id;
+        const result = await db.query(
+            "SELECT * FROM Articles WHERE articleid = $1", [articleId]
+        );
+
+        let blog = result.rows;
+
+        res.render("blog/article.ejs", {
+            activePage: "latest",
+            article: blog[0]
+        });
+    } catch (error) {
+      res.status(500).send("Error fetching post");
+    }
+});
+
+//// Route to render the edit page
+app.get("/edit/:id", async (req, res) => {
+    try {
+        const articleId = req.params.id;
+        const result = await db.query(
+            "SELECT * FROM Articles WHERE articleid = $1", [articleId]
+        );
+
+        let blog = result.rows;
+
+        res.render("blog/modify.ejs", {
+            heading: "Edit Post",
+            submit: "Update Post",
+            activePage: "latest",
+            article: blog[0]
+        });
+    } catch (error) {
+      res.status(500).send("Error fetching post");
+    }
+});
+
+// Partially update a post
+app.post("/update-posts/:id", async (req, res) => {
+    const postTitle = req.body.postTitle;
+    const postBody = req.body.postBody;
+    const postImageURL = req.body.postImageURL;
+    const authorName = req.body.authorName;
+    const articleId = req.params.id;
+
+    try {
+        await db.query(
+            "UPDATE Articles SET title = $1, content = $2, imageurl = $3, authorname = $4, lastmodifieddate = NOW() WHERE articleid = $5",
+            [postTitle, postBody, postImageURL, authorName, articleId]
+        );
+        res.redirect("/");
+    } catch (error) {
+      res.status(500).json({ message: "Error updating post" });
+    }
+});
+
+// Delete a post
+app.post("/delete/:id", async (req, res) => {
+    const id = req.params.id;
+    try {
+      await db.query("DELETE FROM articles WHERE articleid = $1", [id]);
+      res.redirect("/");
+    } catch (err) {
+        res.status(500).json({ message: "Error deleting post" });
+    }
+});
+  
+
+//Add a featured player
+app.post("/update-featured-player", async (req, res) => {
+
+    try {
+        await db.query("DELETE FROM featured_players");
+
+        await db.query(`
+            INSERT INTO featured_players (playerid)
+            SELECT playerid FROM player
+            ORDER BY RANDOM()
+            LIMIT 3;
+        `);
+        res.redirect("/");
+    } catch (err) {
+        console.log(err);
+        res.status(500).send("Server Error, unable to update featured players");
+    }
+});
+
 app.get("/posts", (req, res) => {
-    res.render("posts.ejs", { activePage: "latest" });
+    res.render("blog/posts.ejs", { activePage: "latest" });
 });
 
 app.get("/watch", (req, res) => {
